2c6af0798e1b08acb24fc12d38fdc8c563777401
Sorochinskiy Dmitriy <nea.dima@tadatuta.com>
2013-11-01 13:28:45 +0200

    Added title to url

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = function(){
   	chrome.browserAction.onClicked.addListener(function(tab) {
-        chrome.tabs.create({url: 'http://dev.tadatuta.com:3000/?url=' + tab.url });
+        chrome.tabs.create({url: 'http://dev.tadatuta.com:3000/?url=' + tab.url + '&' + tab.title});
     });
 };

716d29f463e490819535059af151193e27e95aae
Skrynkovskyy Igor <skrynkovskyy@gmail.com>
2014-08-08 13:23:35 +0300

    spaces and new url

--- bg.js
+++ bg.js
@@ -1,25 +1,25 @@
 chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-		var activeTab = arrayOfTabs[0];
-		var urlForSearch = activeTab.url;
-		var searchPage = 'http://static.airomo.com/widgets/search/results.html';
+  chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+    var activeTab = arrayOfTabs[0];
+    var urlForSearch = activeTab.url;
+    var searchPage = 'http://static.airomo.com/widgets/search-uc/sresults.html';
 
-		var isUrlValid = function( url ) {
-			var urlChecker = /http|https:+/;//TODO: put url RE
-  			return urlChecker.test( url );
-		};
+    var isUrlValid = function( url ) {
+      var urlChecker = /http|https:+/;//TODO: put url RE
+      return urlChecker.test( url );
+    };
 
-		var detectOwnDomains = function( url ) {
-			return url.indexOf( searchPage ) != -1;
-		};
+    var detectOwnDomains = function( url ) {
+      return url.indexOf( searchPage ) !== -1;
+    };
 
-		if( detectOwnDomains( urlForSearch ) ) {
-			alert('Please, use search field to search for applications.');
-		} else {
-			if( !isUrlValid( urlForSearch ) ) {
-				urlForSearch = 'airomo';
-			}
-			window.open( searchPage + '?q=' + urlForSearch);
-		}
-	});
+    if( detectOwnDomains( urlForSearch ) ) {
+      alert('Please, use search field to search for applications.');
+    } else {
+      if( !isUrlValid( urlForSearch ) ) {
+        urlForSearch = 'airomo';
+      }
+      window.open( searchPage + '?q=' + urlForSearch);
+    }
+  });
 });
